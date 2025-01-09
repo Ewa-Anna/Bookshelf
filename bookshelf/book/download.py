886bcd9f6a1a8c5675737dc94cab20cf3a9d6ea6
unknown <ewaanna073@gmail.com>
2025-01-09 20:02:47 +0100

    Download and upload basic functionality

---
+++ bookshelf/book/download.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from django.http import HttpResponse
+
+from .models import Book
+
+
+def download_file(request, file_type):
+    data = Book.objects.all().values()
+    df = pd.DataFrame(data)
+
+    if file_type == 'csv':
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="data.csv"'
+        df.to_csv(path_or_buf=response, index=False)
+    elif file_type == 'xlsx':
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename="data.xlsx"'
+        df.to_excel(response, index=False)
+    
+    return response
---
+++ bookshelf/book/upload.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+from django.shortcuts import render, redirect
+from django.core.files.storage import FileSystemStorage
+
+from .models import Book
+
+
+def upload(request):
+    if request.method=="POST" and request.FILES["file"]:
+        file = request.FILES["file"]
+        fs = FileSystemStorage()
+        filename = fs.save(file.name, file)
+        file_path = fs.path(filename)
+
+        try:
+            if filename.endswith(".csv"):
+                data = pd.read_csv(file_path)
+            elif filename.endswith(".xlsx"):
+                data = pd.read_excel(file_path)
+
+            for _, row in data.iterrows():
+                Book.objects.create(
+                    title=row["title"],
+                    author=row["author"],
+                    description=row["description"]
+                )
+            return redirect("library")
+        except Exception as e:
+            return render(request, "upload.html", {"error": str(e)})
+    
+    return render(request, "upload.html")
