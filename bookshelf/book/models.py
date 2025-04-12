ae0a4119cdccd526d5489c9e712a00ed9e49f330
unknown <ewaanna073@gmail.com>
2025-04-12 12:57:34 +0200

    Formatting

--- bookshelf/book/models.py
+++ bookshelf/book/models.py
@@ -5,7 +5,7 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     description = models.TextField()
-    cover = models.ImageField(upload_to='covers/', blank=True, null=True)
+    cover = models.ImageField(upload_to="covers/", blank=True, null=True)
 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
