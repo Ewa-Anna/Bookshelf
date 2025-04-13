a1266c06b16edf1f1970e01cd6882ff40583b177
unknown <ewaanna073@gmail.com>
2025-04-13 14:07:22 +0200

    Formatting

--- bookshelf/book/download.py
+++ bookshelf/book/download.py
@@ -5,13 +5,17 @@ from .models import Book
 
 
 def download(request, file_type):
-    data = Book.objects.all().values('id', 'title', 'author', 'description', 'updated', 'created')
+    data = Book.objects.all().values(
+        "id", "title", "author", "description", "updated", "created"
+    )
     df = pd.DataFrame(data)
 
     if file_type == "csv":
         response = HttpResponse(content_type="text/csv")
         response["Content-Disposition"] = 'attachment; filename="data.csv"'
-        df.to_csv(path_or_buf=response, index=False, lineterminator='\n',encoding='utf-8')
+        df.to_csv(
+            path_or_buf=response, index=False, lineterminator="\n", encoding="utf-8"
+        )
 
     else:
         return HttpResponse("Unsupported file type.", status=400)
