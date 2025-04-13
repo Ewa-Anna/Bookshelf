de32d549c9c6371c537cfde2c4ab099121040d9e
unknown <ewaanna073@gmail.com>
2025-04-13 14:11:43 +0200

    Excel download

--- bookshelf/book/download.py
+++ bookshelf/book/download.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 import pandas as pd
 from django.http import HttpResponse
 
@@ -16,6 +17,19 @@ def download(request, file_type):
         df.to_csv(
             path_or_buf=response, index=False, lineterminator="\n", encoding="utf-8"
         )
+
+    elif file_type == "xlsx":
+        df_copy = df.copy()
+        for col in df_copy.select_dtypes(include=["datetimetz"]).columns:
+            df_copy[col] = df_copy[col].dt.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+
+        output = BytesIO()
+        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+            df_copy.to_excel(writer, index=False, sheet_name='Sheet1')
+
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename="data.xlsx"'
+        response.write(output.getvalue())
 
     else:
         return HttpResponse("Unsupported file type.", status=400)
